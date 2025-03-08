b577ee1b1979617e097728a96d973bf8d9585878
guy arbily <guy.arbilly@gmail.com>
2025-03-08 21:39:10 +0200

    chapter 10 section 204 - back to work with singals- part 1

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,24 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
-import { AsyncPipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
-  imports: [AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService)
-  messages$ = this.messagesService.messages$
+  get messages() {
+    return this.messagesService.allMessages
+  }
   
-  messages : string[] = []
-
-
-
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,18 +1,13 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  private messages: string[] = [];
-  public messages$ = new BehaviorSubject<string[]>([]);
-  get allMessages() {
-    return [...this.messages];
-  }
+  private messages = signal<string[]>([]);
+  allMessages = this.messages.asReadonly();
 
   addMessage(message: string) {
-    this.messages = [... this.messages, message];
-    this.messages$.next([...this.messages]);
+    this.messages.update((prevMessages) => [...prevMessages, message]);
   }
 }
