7342083977a2f145b79044ea0524ab150823b823
guy arbily <guy.arbilly@gmail.com>
2025-03-08 21:25:25 +0200

    chapter 10 section 202 - complete solution with rxjs subject to trigger change detection mark for change

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-messages-list',
@@ -8,12 +9,25 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit{
   private messagesService = inject(MessagesService)
-  get messages() {
-    return this.messagesService.allMessages
+  private cdRef = inject(ChangeDetectorRef) // change detection will be triggered manually
+  private destroyRef = inject(DestroyRef)
+  // get messages() {
+  //   return this.messagesService.allMessages
+  // }
+  messages : string[] = []
+  ngOnInit(): void {
+    const onMessagesSubscription = this.messagesService.messages$.subscribe((response) => {
+      this.cdRef.markForCheck();
+      this. messages = response
+    });
+    this.destroyRef.onDestroy(() => { // will be impl;mented when component is destroyed
+      onMessagesSubscription?.unsubscribe();
+    })
   }
-  
+
+
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
